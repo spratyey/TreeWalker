fcbb41113c6453e8ad3a3beb790c93f8ba0f2a3f
guptarohan6502 <82900156+guptarohan6502@users.noreply.github.com>
2021-05-01 20:21:02 +0530

    Update AdjList.h

--- AdjList.h
+++ AdjList.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "PriorityQueue.h"
-struct node *createNode(int statenum, int val, int parentnum);
+struct node *createNode(struct node *AdjacencyListArray[],int statenum, int val, int parentnum);
 void printAdjacencyList(struct node *AdjacencyListArray[], int maxnode);
 void PushInAdjacencyListarray(struct node *AdjacencyListArray[], int statenum, int val, int parentnum);
 void deleteAdjacencyList(struct node *AdjacencyListArray[], int maxnode);
